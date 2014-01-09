f0fe33f80b55d568a31f755880b6bb7d300ea459
David Higginbotham <davehigginbotham@gmail.com>
2014-01-08 22:06:42 -0700

    goes a level deep

--- test/index.js
+++ test/index.js
@@ -13,33 +13,33 @@ describe('css parser', function () {
       expect(err).to.be(null);
       expect(data).to.be.an(Object);
       
-      expect(data['.className'].background).to.eql('red');
-      expect(data['.className'].color).to.eql('#444000');
-      expect(data['.className'].border).to.eql('1px solid #B4DA55');
+      expect(data['className'].background).to.eql('red');
+      expect(data['className'].color).to.eql('#444000');
+      expect(data['className'].border).to.eql('1px solid #B4DA55');
       
-      expect(data['#idName'].background).to.eql('red');
-      expect(data['#idName'].color).to.eql('#444000');
-      expect(data['#idName'].border).to.eql('1px solid #B4DA55');
+      expect(data['idName'].background).to.eql('red');
+      expect(data['idName'].color).to.eql('#444000');
+      expect(data['idName'].border).to.eql('1px solid #B4DA55');
       
       expect(data['elementName'].background).to.eql('red');
       expect(data['elementName'].color).to.eql('#444000');
       expect(data['elementName'].border).to.eql('1px solid #B4DA55');
       
-      expect(data['.class-name'].background).to.eql('red');
-      expect(data['.class-name'].color).to.eql('#444000');
-      expect(data['.class-name'].border).to.eql('1px solid #B4DA55');
+      expect(data['class-name'].background).to.eql('red');
+      expect(data['class-name'].color).to.eql('#444000');
+      expect(data['class-name'].border).to.eql('1px solid #B4DA55');
       
-      expect(data['#id-name'].background).to.eql('red');
-      expect(data['#id-name'].color).to.eql('#444000');
-      expect(data['#id-name'].border).to.eql('1px solid #B4DA55');
+      expect(data['id-name'].background).to.eql('red');
+      expect(data['id-name'].color).to.eql('#444000');
+      expect(data['id-name'].border).to.eql('1px solid #B4DA55');
       
-      expect(data['.oo.class'].background).to.eql('red');
-      expect(data['.oo.class'].color).to.eql('#444000');
-      expect(data['.oo.class'].border).to.eql('1px solid #B4DA55');
+      expect(data['ooclass'].background).to.eql('red');
+      expect(data['ooclass'].color).to.eql('#444000');
+      expect(data['ooclass'].border).to.eql('1px solid #B4DA55');
       
-      expect(data['.sm .css'].background).to.eql('red');
-      expect(data['.sm .css'].color).to.eql('#444000');
-      expect(data['.sm .css'].border).to.eql('1px solid #B4DA55');
+      expect(data['smcss'].background).to.eql('red');
+      expect(data['smcss'].color).to.eql('#444000');
+      expect(data['smcss'].border).to.eql('1px solid #B4DA55');
 
       return done();
 
