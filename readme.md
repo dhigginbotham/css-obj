a404f78fb574e62f3f58ff5e8eaf0f749cf564f9
David Higginbotham <davehigginbotham@gmail.com>
2014-01-08 20:56:49 -0700

    init

---
+++ readme.md
@@ -0,0 +1,2 @@
+# CSS Object
+Very rough draft of a simple CSS file parser, written in Node.js but meant to play well with Rhino, there isn't much of an API right now so dont waste your time, examples in `test/`.
---
+++ test/index.js
@@ -0,0 +1,53 @@
+var css = require('..');
+var expect = require('expect.js');
+
+describe('css parser', function () {
+  
+  it('should take a css file and turn it into an object', function (done) {
+
+    var file = './style.css';
+
+    css(file, function (err, data) {
+      
+      if (err) return done();
+
+      expect(err).to.be(undefined);
+      
+      expect(data).to.be.an(Object);
+      
+      expect(data['.className'].background).to.eql('red');
+      expect(data['.className'].color).to.eql('#444000');
+      expect(data['.className'].border).to.eql('1px solid #B4DA55');
+      
+      expect(data['#idName'].background).to.eql('red');
+      expect(data['#idName'].color).to.eql('#444000');
+      expect(data['#idName'].border).to.eql('1px solid #B4DA55');
+      
+      expect(data['elementName'].background).to.eql('red');
+      expect(data['elementName'].color).to.eql('#444000');
+      expect(data['elementName'].border).to.eql('1px solid #B4DA55');
+      
+      expect(data['.class-name'].background).to.eql('red');
+      expect(data['.class-name'].color).to.eql('#444000');
+      expect(data['.class-name'].border).to.eql('1px solid #B4DA55');
+      
+      expect(data['#id-name'].background).to.eql('red');
+      expect(data['#id-name'].color).to.eql('#444000');
+      expect(data['#id-name'].border).to.eql('1px solid #B4DA55');
+      
+      expect(data['.oo.class'].background).to.eql('red');
+      expect(data['.oo.class'].color).to.eql('#444000');
+      expect(data['.oo.class'].border).to.eql('1px solid #B4DA55');
+      
+      expect(data['.sm .css'].background).to.eql('red');
+      expect(data['.sm .css'].color).to.eql('#444000');
+      expect(data['.sm .css'].border).to.eql('1px solid #B4DA55');
+
+      return done();
+
+    });
+
+
+  });
+
+});
